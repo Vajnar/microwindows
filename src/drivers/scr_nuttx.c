356fcc6af5c6393d9468ac7464fad6ca90c9887b
Martin Vajnar <martin.vajnar@gmail.com>
2026-09-14 16:37:02 +0200

    scr_nuttx.c: Add pixel type mapping between NuttX and Microwindows
    Signed-off-by: Martin Vajnar <martin.vajnar@gmail.com>

--- src/drivers/scr_nuttx.c
+++ src/drivers/scr_nuttx.c
@@ -39,6 +39,39 @@ SCREENDEVICE scrdev = {
   NULL
 };
 
+static int screen_pixtype(uint8_t nuttx_pixtype)
+{
+  switch(nuttx_pixtype)
+    {
+      case FB_FMT_Y1:
+      case FB_FMT_Y2:
+      case FB_FMT_Y4:
+      case FB_FMT_Y8:
+      case FB_FMT_Y16:
+        return MWPF_PIXELVAL;
+      case FB_FMT_RGB4:
+        return MWPF_PALETTE;
+      case FB_FMT_RGB8:
+        return MWPF_PALETTE;
+      case FB_FMT_RGB8_222:
+        return -1;
+      case FB_FMT_RGB8_332:
+        return MWPF_TRUECOLOR332;
+      case FB_FMT_RGB12_444:
+        return -1;
+      case FB_FMT_RGB16_555:
+        return MWPF_TRUECOLOR555;
+      case FB_FMT_RGB16_565:
+        return MWPF_TRUECOLOR565;
+      case FB_FMT_RGB24:
+        return MWPF_TRUECOLORRGB;
+      case FB_FMT_RGB32:
+        return MWPF_TRUECOLORARGB;
+      default:
+        return -1;
+    }
+}
+
 static PSD fb_open(PSD psd)
 {
   struct fb_videoinfo_s vinfo;
@@ -76,7 +109,13 @@ static PSD fb_open(PSD psd)
   psd->flags = PSF_SCREEN | PSF_ADDRMMAP;
   psd->portrait = MWPORTRAIT_NONE;
 
-  psd->pixtype = SCREEN_PIXTYPE;
+  psd->pixtype = screen_pixtype(vinfo.fmt);
+  if (psd->pixtype < 0)
+    {
+      EPRINTF("Error: unsupported pixel type\n");
+      goto fail;
+    }
+
   psd->data_format = set_data_format(psd);
 
   psd->addr = mmap(NULL, psd->size, PROT_READ | PROT_WRITE, MAP_SHARED, fb, 0);
